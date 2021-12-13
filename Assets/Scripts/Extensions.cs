7374a88510606c52f46ea549094af6e69df74a84
0dminnimda <0dminnimda@gmail.com>
2021-12-13 13:48:44 +0300

    Create Extensions.cs

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public static class Extensions
+{
+    public static float ManhattanDist(this Vector3 a, Vector3 b)
+    {
+        checked
+        {
+            return Mathf.Abs(a.x - b.x) + Mathf.Abs(a.y - b.y) + Mathf.Abs(a.z - b.z);
+        }
+    }
+}
