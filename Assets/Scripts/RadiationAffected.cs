24b490cae9bc858b983e8f7421f2fbbbc8b17145
0dminnimda <0dminnimda@gmail.com>
2021-12-13 17:21:55 +0300

    Create RadiationAffected.cs

---
+++ Assets/Scripts/RadiationAffected.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadiationAffected : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AffectByRadiation(float radiationLevel)
+    {
+        Debug.LogFormat("Affected by radiation with level of {0}", radiationLevel);
+    }
+}
