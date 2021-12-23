b73c318f31112071b31a67713fd759f311ed90f1
0dminnimda <0dminnimda@gmail.com>
2021-12-23 22:32:42 +0300

    Create Difficulty.cs

---
+++ Assets/Scripts/Difficulty.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface DifficultyCurve
+{
+    public float GetDifficulty(int x);
+}
+
+public class Difficulty : MonoBehaviour, DifficultyCurve
+{
+    public float a;
+    
+    public float GetDifficulty(int x)
+    {
+        return a * x;
+    }
+}
