a8060dd539d5be78c0929eaa482c76230ff6cbdc
0dminnimda <0dminnimda@gmail.com>
2021-12-24 02:27:40 +0300

    Move related scropts to Radiation\

---
+++ Assets/Scripts/Radiation/RadiationAffected.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Radiation
+{
+    public class RadiationAffected : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void AffectByRadiation(float radiationLevel)
+        {
+            // Debug.LogFormat("SUPASUS, Affected by radiation with level of {0}", radiationLevel);
+        }
+    }
+}
