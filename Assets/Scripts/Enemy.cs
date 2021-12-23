25f56efd2664bc024168ad197bf6d24dc1fe9382
0dminnimda <0dminnimda@gmail.com>
2021-12-24 02:16:21 +0300

    Enemy.cs: goes towards 0,0,0

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,10 @@
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, Vector3.zero, .05f);
+    }
 }
