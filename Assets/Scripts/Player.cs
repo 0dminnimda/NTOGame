a6e88415709d90ad85f60c597ba7231dae1192ed
0dminnimda <0dminnimda@gmail.com>
2021-12-25 05:54:26 +0300

    Create Player.cs

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject[] turets;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+        }
+    }
+}
